06661295681cf675a6a21d08e85d3bc3f07d9aa1
rchamacfree <rchamacfree@gmail.com>
2023-10-17 13:09:00 +0200

    lanzamos query

--- sprint2apache/www/mysite/main.php
+++ sprint2apache/www/mysite/main.php
@@ -2,5 +2,28 @@
 $db = mysqli_connect("localhost","root","2253","peliculas") or die("fail");
 ?>
 
-<h1>Conexion establecida</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Conexión establecida.</h1>
+    <?php
+    //Lanzamos una query
+    $query = 'SELECT * FROM tpeliculas';
+    mysqli_query($db, $query) or die("query error");
+    //Recorremos la query
+    while ($row = mysqli_fetch_array($result)){
+        echo $row['nombre'];
+        echo '<br>';
+        echo $row['director'];
+        echo '<br>?';
+    }
+
+    ?>
+</body>
+</html>
 
